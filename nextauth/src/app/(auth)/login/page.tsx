49e2edf381250899a460a218b29aebbe3fafbee9
David Kopelent <davidekkopelent@gmail.com>
2025-03-28 01:06:58 +0100

    style: UI modifications

--- nextauth/src/app/(auth)/login/page.tsx
+++ nextauth/src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation';
-import { auth, signIn } from '@/auth';
+import { auth } from '@/auth';
+import LoginForm from '@/src/components/LoginForm';
 
 export default async function Login() {
     const session = await auth();
@@ -8,13 +9,6 @@ export default async function Login() {
     }
 
     return (
-        <div className="block w-full max-w-[500px] m-auto sm-md:rounded-3xl bg-white sm-md:border sm-md:border-slate-100 dark:border-dark-secondary">
-            <h2 className="block w-full text-center text-xl font-semibold border-b border-slate-100 dark:border-dark-secondary p-4">Login</h2>
-            <form action={signIn}> 
-                <input type="email" name="email" placeholder="Email" />
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <LoginForm />
     );
 }
